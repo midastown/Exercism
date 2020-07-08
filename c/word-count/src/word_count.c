26310fe42025fae2cbd146e1f186ef85e5b019e3
kaisu <mehdihachimi1@gmail.com>
2020-07-08 17:42:11 +0100

    passed first test ...finally

--- c/word-count/src/word_count.c
+++ c/word-count/src/word_count.c
@@ -5,12 +5,6 @@
 #include <stdio.h>
 
 int word_count(const char *input_text, word_count_word_t * words) {
-    
-    //strncpy(words[0].text, input_text, MAX_WORD_LENGTH);
-    //words[0].count = 1;
-    
-    //return 1;
-
     // 1) loop through the input_text char by char
     // a) char word[] = ""; empty word
     // b) check if char = space, then that means it's the end of word
@@ -21,13 +15,12 @@ int word_count(const char *input_text, word_count_word_t * words) {
     // 3) keep a count of words of EACH INDIVIDUAL WORD as index of words
     int words_index = 0, word_index = 0, i, j;
     int len = strlen(input_text);
-    char word[MAX_WORD_LENGTH + 1];
-    printf("%d\n", len);
+    char word[MAX_WORD_LENGTH + 1] = "";
 
-    for (i=0; i < len; i++) {
-        printf("value of input_text[i]: %c\n", input_text[i]);
+    for (i=0; i <= len; i++) {
         if (input_text[i] == ' ' || input_text[i] == '\0') {
-            printf("this condition actually works");
+            printf("%s\n", word);
+
             int yes = 0;
             for (j=0; j < words_index+1; j++) {
                 if (strcmp(word, words[j].text) == 0) {
@@ -37,7 +30,7 @@ int word_count(const char *input_text, word_count_word_t * words) {
                 }
             }
             if (yes==0) {
-                strncpy(words[words_index].text, word, MAX_WORD_LENGTH);
+                strcpy(words[words_index].text, word);
                 words[words_index].count = 1;
                 words_index++;
                 strcpy(word, "");
@@ -48,7 +41,6 @@ int word_count(const char *input_text, word_count_word_t * words) {
             word_index++;
         }
     }
-    printf("this should be filled: %s\n", words[0].text);
 
     return words_index;
 }
